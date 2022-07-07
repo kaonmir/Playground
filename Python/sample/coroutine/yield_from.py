c91af7f28964321d0118c2243d593530c62a3266
kaonmir <thswpvm@cau.ac.kr>
2022-07-07 13:05:49 +0900

    refactor python and add yield command

---
+++ Python/sample/coroutine/yield_from.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def return_one_to_ten():
+    for i in range(10):
+        yield i
+
+
+def get_coroutine():
+    yield from return_one_to_ten()
+
+
+def main():
+    print("=== Get coroutine ===")
+    c = get_coroutine()
+    print(c)
+
+    print("=== Get values ===")
+    print(next(c))
+    print(list(c))
+
+
+if __name__ == "__main__":
+    main()
