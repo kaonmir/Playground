e8473cdc3f4584f7fcac30b9c70fe30983434fc3
kaonmir <thswpvm@cau.ac.kr>
2022-07-20 18:21:01 +0900

    implement email notification by Python with Celery

--- Python/practice/celery-practice/son_app.py
+++ Python/practice/celery-practice/son_app.py
@@ -1,9 +1,23 @@
 from celery import Celery
 import smtplib
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+RABBITMQ_URL = os.getenv("RABBITMQ_URL")
+if RABBITMQ_URL is None:
+    raise Exception("Please set RABBITMQ_URL environment variable.")
+
 
 app = Celery(
     "son_tasks",
-    broker="amqp://son:son@192.168.25.40:5672//",
+    broker=RABBITMQ_URL,
     backend="rpc://",
     include=["tasks.calc", "tasks.email", "tasks.log"],
 )
+
+smtp_gmail = smtplib.SMTP("smtp.gmail.com", 587)
+smtp_gmail.ehlo()
+smtp_gmail.starttls()
+smtp_gmail.login("thswpvm1111@gmail.com", "wajiruayokplkhzm")
--- Python/practice/celery-practice/tasks/email.py
+++ Python/practice/celery-practice/tasks/email.py
@@ -1,9 +1,24 @@
 from random import randint
 import time
-from son_app import app
-import smtplib
+from son_app import app, smtp_gmail
+from email.message import EmailMessage
 
 
 @app.task
 def notify(message):
+    msg = EmailMessage()
+    msg["Subject"] = "제목입니다."
+    msg.set_content(f"{str(message)}가 정답입니까?")
+    msg["From"] = "thswpvm1111@gmail.com"
+    msg["To"] = "sonjeff@naver.com"
+
+    smtp_gmail.send_message(msg)
+
     time.sleep(randint(1, 5))
+
+
+### 아래는 첨부파일 예시 ###
+# file='./test.csv'
+# fp = open(file,'rb')
+# file_data=fp.read()
+# msg.add_attachment(file_data,maintype='text',subtype='plain',filename="test.csv")
