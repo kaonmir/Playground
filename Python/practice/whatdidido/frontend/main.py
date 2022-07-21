0c2d6f85f43bf403f302b966f180c9c59883231e
kaonmir <thswpvm@cau.ac.kr>
2022-07-21 21:56:12 +0900

    hide url from outside

--- Python/practice/whatdidido/frontend/main.py
+++ Python/practice/whatdidido/frontend/main.py
@@ -3,10 +3,19 @@
 from flask import Flask
 from celery import Celery
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+RABBITMQ_URL = os.getenv("RABBITMQ_URL")
+if RABBITMQ_URL is None:
+    raise Exception("Please set RABBITMQ_URL environment variable.")
+
 
 celery = Celery(
     "tasks",
-    broker="amqp://son:son@192.168.25.40:5672//",
+    broker=RABBITMQ_URL,
     backend="rpc://",
     include=["tasks.calc"],  # ["tasks.calc", "tasks.email", "tasks.log"]
 )
@@ -22,4 +31,4 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=80)
