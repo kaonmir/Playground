17cc89308f521514e256e31904dce3c758c63c9d
kaonmir <thswpvm@cau.ac.kr>
2022-07-22 10:31:19 +0900

    add email to celery

--- Python/practice/whatdidido/backend/celery_app.py
+++ Python/practice/whatdidido/backend/celery_app.py
@@ -8,6 +8,12 @@ load_dotenv()
 RABBITMQ_URL = os.getenv("RABBITMQ_URL")
 if RABBITMQ_URL is None:
     raise Exception("Please set RABBITMQ_URL environment variable.")
+GMAIL_ADDRESS = os.getenv("GMAIL_ADDRESS")
+GMAIL_CREDENTIAL = os.getenv("GMAIL_CREDENTIAL")
+if GMAIL_ADDRESS is None or GMAIL_CREDENTIAL is None:
+    raise Exception(
+        "Please set GMAIL_ADDRESS and GMAIL_CREDENTIAL environment variables."
+    )
 
 
 celery = Celery(
@@ -16,3 +22,10 @@ celery = Celery(
     backend="rpc://",
     include=["tasks.calc", "tasks.notificate"],
 )
+
+import smtplib
+
+smtp_gmail = smtplib.SMTP("smtp.gmail.com", 587)
+smtp_gmail.ehlo()
+smtp_gmail.starttls()
+smtp_gmail.login(GMAIL_ADDRESS, GMAIL_CREDENTIAL)
--- Python/practice/whatdidido/backend/tasks/notificate.py
+++ Python/practice/whatdidido/backend/tasks/notificate.py
@@ -1,6 +1,15 @@
-from celery_app import celery
+from celery_app import celery, smtp_gmail
+from email.message import EmailMessage
 
 
 @celery.task
 def notifyToEmailFromResult(result: int):
-    return result
+    msg = EmailMessage()
+    msg["Subject"] = "제목입니다."
+    msg.set_content(f"{str(result)}가 정답입니까?")
+    msg["From"] = "thswpvm1111@gmail.com"
+    msg["To"] = "sonjeff@naver.com"
+
+    smtp_gmail.send_message(msg)
+
+    return -143
